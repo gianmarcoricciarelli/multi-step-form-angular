cc2b4101f8d015b79fd5499eb3c89452d7c8e65f
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2025-01-28 19:08:54 +0100

    feature: init first-step

---
+++ src/app/form-body/form-content/first-step/first-step.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first-step',
+  imports: [],
+  templateUrl: './first-step.component.html',
+  styleUrl: './first-step.component.scss'
+})
+export class FirstStepComponent {
+
+}
