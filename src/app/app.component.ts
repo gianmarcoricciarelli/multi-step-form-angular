bacc0253dcc4a696dbfeea6ec084c9b3bb9aeebc
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2025-01-28 15:32:11 +0100

    feature: scaffolding and form's stepper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core'
+import { StepperComponent } from './stepper/stepper.component'
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
+    imports: [StepperComponent],
 })
 export class AppComponent {
     title = 'multi-step-form'
---
+++ src/app/stepper/stepper.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-stepper',
+    imports: [],
+    templateUrl: './stepper.component.html',
+    styleUrl: './stepper.component.scss',
+})
+export class StepperComponent {}
