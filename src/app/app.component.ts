3864e3f555061ebcc7ea7c40328d5093380a8afe
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2025-01-28 14:46:40 +0100

    feature: init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'multi-step-form';
+    title = 'multi-step-form'
 }
